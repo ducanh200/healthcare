a4736e46ec16126525aa984811e0e3092c3f19c2
Hieunmb <zhuongdae@gmail.com>
2024-03-12 10:04:11 +0700

    css, js package, layout and 3 section home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/layouts/header';
+import Footer from './components/layouts/footer';
+import Home from './components/pages/home';
+import Home1 from './components/pages/home1';
+import Home2 from './components/pages/home2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="main-wrapper">
+        <Header></Header>
+        <Home></Home>
+        <Home1></Home1>
+        <Home2></Home2>
+        <Footer></Footer>
+      <div class="mouse-cursor cursor-outer"></div>
+<div class="mouse-cursor cursor-inner"></div>
+      </div>
+      <div class="progress-wrap active-progress">
+<svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style={{transition: 'stroke-dashoffset 10ms linear 0s', strokeDasharray: '307.919px, 307.919px', strokeDashoffset: '228.265px'}}></path>
+</svg>
+</div>
     </div>
   );
 }
