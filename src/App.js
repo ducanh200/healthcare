11e2e07c128d848d7840e33946cf7ebbd4397b4f
me0giazodoi <hiepga1243@gmail.com>
2024-03-15 16:51:09 +0700

    format

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
 import Header from './components/layouts/header';
 import Footer from './components/layouts/footer';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import BreadCrumb from './components/layouts/breadcrumb';
+
 import Home from './components/pages/home';
 import Booking from './components/pages/booking';
-import BreadCrumb from './components/layouts/breadcrumb';
 
 function App() {
   const location = useLocation();
@@ -23,14 +25,14 @@ function App() {
           <Route path='/booking' element={<Booking/>}/>
         </Routes>
         <Footer></Footer>
-      <div class="mouse-cursor cursor-outer"></div>
-<div class="mouse-cursor cursor-inner"></div>
+        <div class="mouse-cursor cursor-outer"></div>
+        <div class="mouse-cursor cursor-inner"></div>
       </div>
       <div class="progress-wrap active-progress">
-<svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
-<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style={{transition: 'stroke-dashoffset 10ms linear 0s', strokeDasharray: '307.919px, 307.919px', strokeDashoffset: '228.265px'}}></path>
-</svg>
-</div>
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+          <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style={{transition: 'stroke-dashoffset 10ms linear 0s', strokeDasharray: '307.919px, 307.919px', strokeDashoffset: '228.265px'}}></path>
+        </svg>
+      </div>
     </div>
   );
 }
