01e9c5e2834f49326bd14aa42da495ba1edc1867
Hieunmb <zhuongdae@gmail.com>
2024-04-15 15:01:42 +0700

    department merge

--- src/components/pages/department.js
+++ src/components/pages/department.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import api from "../../services/api";
 import url from "../../services/url";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Department() {
+    const navigate = useNavigate();
     const [departments, setDepartments] = useState([]);
 
     useEffect(() => {
@@ -17,52 +18,56 @@ function Department() {
         };
         loadDepartment();
     }, []);
+    const handleClick = (departmentId) => {
+        navigate(`/booking/${departmentId}`)
+        window.location.reload()
+    };
 
     return (
-        <div class="content">
-<div class="container">
-<div class="row">
+        <div className="content">
+<div className="container">
+<div className="row">
 
 
 </div>
-<div class="col-md-12 col-lg-12 col-xl-12">
+<div className="col-md-12 col-lg-12 col-xl-12">
 {departments.map((department, index) => (
-<div key={index} class="card">
-<div class="card-body">
-<div class="doctor-widget">
-<div class="doc-info-left">
-<div class="doctor-img">
+<div key={index} className="card">
+<div className="card-body">
+<div className="doctor-widget">
+<div className="doc-info-left">
+<div className="doctor-img">
 <a href="doctor-profile.html">
-<img src={department.thumbnail} class="img-fluid" alt="User Image"/>
+<img src={department.thumbnail} className="img-fluid" alt="User Image"/>
 </a>
 </div>
-<div class="doc-info-cont">
-<h4 class="doc-name"><a href="doctor-profile.html">{department.name}</a></h4>
-<p class="doc-speciality">{department.description}
+<div className="doc-info-cont">
+<h4 className="doc-name"><a href="doctor-profile.html">{department.name}</a></h4>
+<p className="doc-speciality">{department.description}
 </p>
-<div class="clinic-details">
-<p class="doc-location"><i class="fas fa-map-marker-alt"></i> 8a Ton That Thuyet</p>
+<div className="clinic-details">
+<p className="doc-location"><i className="fas fa-map-marker-alt"></i> 8a Ton That Thuyet</p>
 
 </div>
 </div>
 </div>
-<div class="doc-info-right">
-<div class="clini-infos">
+<div className="doc-info-right">
+<div className="clini-infos">
 <ul>
-<li><i class="far fa-money-bill-alt"></i> ${department.expense} <i class="fas fa-info-circle" data-bs-toggle="tooltip" title="Lorem Ipsum"></i> </li>
+<li><i className="far fa-money-bill-alt"></i> ${department.expense} <i className="fas fa-info-circle" data-bs-toggle="tooltip" title="Lorem Ipsum"></i> </li>
 </ul>
 </div>
-<div class="clinic-booking">
-<a class="view-pro-btn" href="doctor-profile.html">View Profile</a>
-<Link class="apt-btn" to={`/booking/${department.id}`}>Book Appointment</Link>
+<div className="clinic-booking">
+<a className="view-pro-btn" href="doctor-profile.html">View Profile</a>
+<a className="apt-btn" onClick={() => handleClick(department.id)}>Book Appointment</a>
 </div>
 </div>
 </div>
 </div>
 </div>
 ))}
-<div class="load-more text-center">
-<a class="btn btn-primary btn-sm prime-btn" href="javascript:void(0);">Load More</a>
+<div className="load-more text-center">
+<a className="btn btn-primary btn-sm prime-btn" href="javascript:void(0);">Load More</a>
 </div>
 </div>
 </div>
