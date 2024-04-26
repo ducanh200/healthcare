7a8e57e094f88cfe2ab4d681936c697ca5302454
Hieunmb <zhuongdae@gmail.com>
2024-04-26 17:42:45 +0700

    calendar and url fix

--- src/services/url.js
+++ src/services/url.js
@@ -1,9 +1,9 @@
 const url ={
     BASE_URL:"http://localhost:8080/api/v3",
     PATIENT:{
-        REGISTER:"/patients",
-        LOGIN:"/patients/login",
-        PROFILE:"/patients/profile"
+        REGISTER:"auth/patients",
+        LOGIN:"auth/patients/login",
+        PROFILE:"auth/patients/profile"
     },
     DEPARTMENT:{
         LIST: "/departments",
